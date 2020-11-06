8fb5d340c6f581f0364e44f87ae8900efa7093a2
bolwoy97 <bolwoy97@gmail.com>
2020-11-06 14:30:18 +0200

    another commit

--- components/dbinit.js
+++ components/dbinit.js
@@ -1,37 +1,26 @@
-// Инициализация датабазы!
-// Загрузим mongoose
 var mongoose = require('mongoose');
-// Заменим библиотеку Обещаний (Promise) которая идет в поставку с mongoose (mpromise)
+const dotenv = require('dotenv').config();
 mongoose.Promise = require('bluebird');
-// На Bluebird
-// Подключимся к серверу MongoDB
-// В дальнейшем адрес сервера будет загружаться с конфигов
 
 mongoose.connection.on('error',(err)=>
 {
     console.error("Database Connection Error: " + err);
-    // Скажите админу пусть включит MongoDB сервер :)
     process.exit(2);
 });
 
-// Данная функция будет вызвано когда подключение будет установлено
 mongoose.connection.on('connected',()=>
 {
-    // Подключение установлено
     console.log("Succesfully connected to MongoDB Database");
-    // В дальнейшем здесь мы будем запускать сервер.
 });
 
 connect = async function () {
     await mongoose.connect(
-      //'mongodb://localhost:27017/todos',
-      'mongodb+srv://nma_user:bU63zYj2RvtbZju@cluster0.szpx7.mongodb.net/todos',
+        process.env.MONGO_CONNECTION_STR,
       {
         useNewUrlParser: true,
         useFindAndModify: false
       }
     )
-    
 }
 
 module.exports.connect = connect
