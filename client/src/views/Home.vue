8b6600016202ab4eae5a83e0e5b5cb15200f7149
bolwoy97 <bolwoy97@gmail.com>
2020-11-09 10:48:32 +0200

    another commit

--- client/src/views/Home.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <h2>Home page</h2>
-
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dignissimos eveniet excepturi expedita impedit numquam quae quas tenetur velit voluptas!</p>
-
-    <router-link to="/todos">Todos</router-link>
-  </div>
-</template>
